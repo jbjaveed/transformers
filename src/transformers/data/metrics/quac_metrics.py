d4889abedfbfbdf0ac642506880a5d9ea5ed5b1b
JaveedSpritle <javeed.b@spritle.com>
2020-05-23 12:53:55 +0530

    progress

--- src/transformers/data/metrics/quac_metrics.py
+++ src/transformers/data/metrics/quac_metrics.py
@@ -384,6 +384,8 @@ def compute_predictions_logits(
     tokenizer,
 ):
     """Write final predictions to the json file and log-odds of null if needed."""
+    print(all_examples,all_results)
+
     if output_prediction_file:
         logger.info(f"Writing predictions to: {output_prediction_file}")
     if output_nbest_file:
