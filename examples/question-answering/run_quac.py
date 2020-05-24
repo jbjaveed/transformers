d0e648eee4b4ca412a5ec4dbef4deaadbb8f7a4a
JaveedSpritle <javeed.b@spritle.com>
2020-05-24 10:03:14 +0530

    progress

--- examples/question-answering/run_quac.py
+++ examples/question-answering/run_quac.py
@@ -770,7 +770,7 @@ def main():
     # Training
     if args.do_train:
         train_dataset = load_and_cache_examples(args, tokenizer, evaluate=False, output_examples=False)
-        exit()
+        # exit()
         global_step, tr_loss = train(args, train_dataset, model, tokenizer)
         logger.info(" global_step = %s, average loss = %s", global_step, tr_loss)
 
